c99e8965746be0bd2bad5256408a17569162c299
Nathan Wandera <96195805+pebblion@users.noreply.github.com>
2025-01-28 23:07:46 -0600

    Add files via upload

--- 1.1/sketch.js
+++ 1.1/sketch.js
@@ -9,9 +9,10 @@ function sketch()
 {
   background(100, 100, 100);
   stroke('black');
+  strokeWeight('1');
   fill(0, 0, 100);
   circle(windowWidth / 4, windowHeight / 2, 500);
-  square((windowWidth / 2), windowHeight / 7, 500);
+  square((windowWidth / 2), windowHeight / 4, 500);
 }
 
 function sketch1()
@@ -20,16 +21,50 @@ function sketch1()
   noStroke();
   fill(0, 100, 100, 0.4);
 
-  circle(windowWidth/2, windowHeight/4, 500);
+  circle(windowWidth/2, windowHeight/4 + 100, 300);
   fill(120, 100, 100, 0.4);
-  circle(windowWidth/2 + 200, windowHeight/2, 500);
+  circle(windowWidth/2 + 100, windowHeight/2, 300);
   fill(240, 100, 100, 0.4);
-  circle(windowWidth/2 - 200, windowHeight/2, 500);
+  circle(windowWidth/2 - 100, windowHeight/2, 300);
 }
 
 function pacman() {
   background(0, 0, 0);
+  noStroke();
+  fill(60, 100, 100);
+  arc(windowWidth/4 + 100, windowHeight/2, 350, 350, 3.9, 2.3);
+  fill(0, 80, 100)
+  circle(windowWidth/2 + 150, windowHeight/2 - 50, 300);
+  rect(windowWidth/2, windowHeight/2 - 50, 300, 200);
+  fill(0, 0, 100);
+  circle(windowWidth/2 + 75, windowHeight/2 - 50, 125);
+  circle(windowWidth/2 + 225, windowHeight/2 - 50, 125);
+  fill(240, 100, 100);
+  circle(windowWidth/2 + 75, windowHeight/2 - 50, 100);
+  circle(windowWidth/2 + 225, windowHeight/2 - 50, 100);
+
+}
+function star(){
+  background(240, 100, 100);
+  fill(120, 100, 50);
+  stroke('white');
+  strokeWeight(10);
+  circle(windowWidth/2, windowHeight/2, 350);
+
+  fill("red");
+  beginShape();
+  vertex(windowWidth/2 - 200, windowHeight/2 - 50);
+  vertex(windowWidth/2 - 50, windowHeight/2 - 50);
+  vertex(windowWidth/2, windowHeight/2 - 200);
+  vertex(windowWidth/2 + 50, windowHeight/2 - 50);
+  vertex(windowWidth/2 + 200, windowHeight/2 - 50);
+  vertex(windowWidth/2 + 70, windowHeight/2 + 40);
+  vertex(windowWidth/2 + 150, windowHeight/2 + 200);
+  vertex(windowWidth/2, windowHeight/2 + 75);
+  vertex(windowWidth/2 - 150, windowHeight/2 + 200);
+  vertex(windowWidth/2 -70, windowHeight/2 + 40);
   
+  endShape(CLOSE);
 }
 function draw() {
   switch(drawing)
@@ -43,10 +78,11 @@ function draw() {
     case 2:
       pacman();
       break;
-    default:
+    case 3:
+      star();
       break;
   }  
-  if(drawing > 2){
+  if(drawing > 3){
     drawing = 0;
   }
 }
