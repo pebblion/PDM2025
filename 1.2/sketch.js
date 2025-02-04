683d0e8e1898a111a39d1b87888997df461ad2ee
Nathan Wandera <96195805+pebblion@users.noreply.github.com>
2025-02-04 11:45:03 -0600

    Add files via upload

---
+++ 1.2/sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(2000,1000);
+}
+
+function draw() {
+  background('white');
+  fill("red");
+  square(0, 0, 50);
+  fill("orange");
+  square(0, 55, 50);
+  fill("yellow");
+  square(0, 110, 50);
+  fill("green");
+  square(0, 165, 50);
+  fill("cyan");
+  square(0, 220, 50);
+  fill("blue");
+  square(0, 275, 50);
+  fill("pink");
+  square(0, 330, 50);
+  fill("brown");
+  square(0, 385, 50);
+  fill("white");
+  square(0, 440, 50);
+  fill("black");
+  square(0, 495, 50);
+
+}
+
+function mousePressed()
+{
+  //color pallete
+  if (mouseX > 0 && mouseX < 50)
+  {
+    //red
+    
+  }
+}
