e1f8e6c782ca6300728f0e89db84e45344831e35
Nathan Wandera <96195805+pebblion@users.noreply.github.com>
2025-02-20 23:49:14 -0600

    Add files via upload

--- 1.4/sketch.js
+++ 1.4/sketch.js
@@ -9,52 +9,19 @@ let score = 0;
 let timer = 30;
 let directions = ["left", "right", "up", "down"];
 let bugSprite;
-let side = false;
-let squish = false;
-let bugs = [];
 
 function preload() {
-  bugSprite = loadImage("media/bugf.png");
+  bugSprite = imageLoad("media/bugf.png");
 }
 function setup() {
-  createCanvas(400, 400);
-
-  for (let i=0; i < 15; i++)
-  { 
-    bugs[i] = new bug(random(32, width-32), random(32, height-32), random(directions), bugSprite);
-  }
+  createCanvas(windowWidth, windowHeight);
+  text("Score:", 50, 20);
+  text("Timer:", 100, 20);
 }
 
 function draw() {
   background(220);
-  textSize(20);
-  text("Score:", 0, 20);
-  text(score, 60, 20);
-  text("Timer:", 100, 20);
-  text(timer, 160, 20);
-  if (frameCount % 60 == 0 && timer > 0) { 
-    timer--;
-  }
-  if (timer <= 0) {gameState = GameStates.END;}
-  
-
- switch(score)
- {
-  case 0:
-    bugs[0].draw();
-    break;
-  case 2:
-    bugs[1].draw();
-    break;
- }
-   
- //console.log(mob1.pos);
-}
-
-function mouseClicked()
-{
-  console.log('clicked');
-  bugs[0].mouseClicked();
+  mob1 = new bug(random(32,width-32),random(32,height-32), "left", bugSprite);
 }
 
 class bug {
@@ -62,25 +29,13 @@ class bug {
   this.x = x;
   this.y = y;
   this.frames = {};
-  this.speed = 0;
   this.direction = direction;
-  this.squished = false;
   this.animation = new SpriteAnimation(sprite, 0, 0, 5);
-  this.squish_animation = new SpriteAnimation(sprite, 5, 0, 1);
-  }
-  
-  mouseClicked()
-  {
-    if (mouseX >= bugs[0].x && mouseX <= bugs[0].x + 20 && mouseY) {bugs[0].squished = true; score++;}
-  }
-
-  get pos() {
-    return [this.x, this.y]
   }
 
   draw()
   {
-    switch(this.direction) {
+    switch(direction) {
       case "left":
         this.x -= 1;
         break;
@@ -94,15 +49,8 @@ class bug {
         this.y += 1;
         break;   
     }
-  
-    translate(this.x, this.y);
-    if (!this.squished) {this.animation.draw();}
-    
-    if(this.squished){this.squish_animation.draw();}
-
+    this.animation.draw();
   }
-
-  
 }
 
 class SpriteAnimation {
@@ -119,8 +67,8 @@ class SpriteAnimation {
   // change sprites direction based on parameter
   draw() {
 
-    //if(side == true) {scale (-1,1);}
-    //else if(side == false){scale(1,1);}
+    if(side == true) {scale (-1,1);}
+    else if(side == false){scale(1,1);}
     image(this.spritesheet, 0, 0, 32, 32, this.u*32, this.v*32, 32, 32);
 
     this.frameCount++;
